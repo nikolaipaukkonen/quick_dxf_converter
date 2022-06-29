e78ff357eea4dc3051251738fe82ddbd9a5d69ea
nikolaipaukkonen <nikolai.paukkonen@gmail.com>
2022-06-29 19:22:22 +0300

    hienosäätöä

--- converter.py
+++ converter.py
@@ -11,7 +11,7 @@ print("Use: python3 proto_converter.py filename_to_be_converted.txt filename_to_
 f = sys.argv[1]
 with open(f) as fp:
     line = fp.readline()
-    data_list = [] #a list of lists where 0=no, 1=x, 2=y, 3=z and 4=code
+    data_list = [] #a list of lists where 0=ptid, 1=x, 2=y, 3=z and 4=code
     cnt = 0
     while line:
         n_x_y_z_code = line.split()
@@ -23,14 +23,17 @@ with r12writer(sys.argv[2]) as dxf:
     polyline_3d = []
     for line in data_list:
         print(line)
-        if (line[4] == "91201") or (line[4] == "81201"): #codes can be altered and added
-            polyline_3d.append((float(line[1]), float(line[2]), float(line[3])))
+        if len(line) == 5:
+            if (line[4] == "91201") or (line[4] == "81201"): #codes can be altered and added
+                polyline_3d.append((float(line[1]), float(line[2]), float(line[3])))
             #print(polyline_3d)
-            dxf.add_polyline(polyline_3d)
-            polyline_3d = []
-        elif line[4] == "11201":
-            polyline_3d.append((float(line[1]), float(line[2]), float(line[3])))
+                dxf.add_polyline(polyline_3d)
+                polyline_3d = []
+            elif line[4] == "11201":
+                polyline_3d.append((float(line[1]), float(line[2]), float(line[3])))
+            else:
+                dxf.add_point((float(line[1]), float(line[2]), float(line[3])), layer=line[0])
         else:
-            dxf.add_point((float(line[1]), float(line[2]), float(line[3])), layer=line[4])
+            dxf.add_point((float(line[1]), float(line[2]), float(line[3])), layer=line[0])
 
-    dxf.add_text(sys.argv[2], align="MIDDLE_CENTER") #prints filename, just testing
+    #dxf.add_text(sys.argv[2], align="MIDDLE_CENTER") #prints filename, just testing
